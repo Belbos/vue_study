83d98062fb39cebc1d44e3f221f8c8bd41d3d958
belbo <maplelyy@naver.com>
2018-09-16 06:08:04 +0900

    요구사항3

--- js/views/FormView.js
+++ js/views/FormView.js
@@ -9,10 +9,20 @@ FormView.setup = function (el){
     this.inputEl = el.querySelector('[type=text]')
     this.resetEl = el.querySelector('[type=reset]')
     this.showResetBun(false)
+    this.bindEvets()
+    return this
 }
 
 FormView.showResetBun = function (show = true) {
     this.resetEl.style.display = show ? 'block' :'none'
+}
+
+FormView.bindEvets = function (){
+    this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
+}
+
+FormView.onKeyup = function (){
+    this.showResetBun(this.inputEl.value.length)
 }
 
 export default FormView
